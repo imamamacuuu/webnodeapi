2f94cde6b09fbf445cb33cd7bc7e8883d4f93bb4
Hgvdwr <101828391+Hgvdwr@users.noreply.github.com>
2022-07-24 10:51:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -818,7 +818,7 @@ case ',sangecek':
 			case ',wibucek':
 			case ',ceknolep':
 			case ',nolepcek':
-			case ',cekbandar':
+			case ',cekbdandar':
 			case ',bandarcek':
 				if (!q) return reply(`Penggunaan ${command} Nama\n\nContoh : ${command} ${pushname}`)
 					const sangeh = ['5', '10', '15','20', '25','30','35','40','45','50','55','60','65','70','75','80','85','90','100']
